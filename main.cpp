3c6e72afd82c7f8dea245f6bdfd634b959ab0c8b
Federico Suarez <suarezfed@gmail.com>
2017-04-23 18:44:39 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,37 @@ using namespace std;
 
 int main()
 {
-    char elemento;
-    cadenas A;
-    cout << "Ingrese elemento" << endl;
-    cin >> elemento;
-    A.imprimir(elemento);
+    cadenas A,B;
+    //char dato;
+    //int pos;
+    //cout << "ingrese un char" << endl;
+    //cin >> dato;
+    //A.agregar(dato);
+    A.agregar('c');
+    A.agregar('j');
+    A.agregar('j');
+    A.agregar('c');
+    //A.reversa();
+    //A.duplicar();
+    if (A.capicua())
+        cout << "es capicua" << endl;
+    else
+        cout << "no es capicua" << endl;
+    A.imprimir();
+    //cout << "la cantidad de j es: " << A.repeticiones('j') << endl;
+    //cout << "Ingrese dato y posicion a insertar" << endl;
+    //cin >> dato >> pos;
+    //A.insertar(pos,dato);
+    //cout << " " << endl; //<----  Me hace un espacio para ver mejor la ejecucion
+    //B.agregar('j');
+    //B.agregar('j');
+    //B.agregar('z');
+    //B.agregar('s');
+    //if (A.iguales(B))
+    //    cout << "iguales" << endl;
+    //else
+    //    cout << "distintas" << endl;
+    //A.imprimir();
+    //cout << "La longitud es: " << A.longitud() << endl;
     return 0;
 }
